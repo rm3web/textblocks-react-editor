17b2c729a8589e87266654464d7ad03bf6719003
Ken Wronkiewicz <wh-github@wirewd.com>
2017-03-11 16:47:32 -0800

    Fix tests.

--- tests/test-textblocks_forms.js
+++ tests/test-textblocks_forms.js
@@ -53,14 +53,14 @@ describe('TextBlockComponent', function() {
 
     var blocklist = form.props.children[1];
     blocklist[0].type.should.equal('div');
-    blocklist[0].props.children.props.prefix.should.equal('plastic_makes_perfect[blocks][0]');
-    blocklist[0].props.children.props.block.should.eql({source:"# humans make good pets",
+    blocklist[0].props.children[1].props.prefix.should.equal('plastic_makes_perfect[blocks][0]');
+    blocklist[0].props.children[1].props.block.should.eql({source:"# humans make good pets",
       format:"markdown"});
     blocklist[0].key.should.equal('0');
 
     blocklist[1].type.should.equal('div');
-    blocklist[1].props.children.props.prefix.should.equal('plastic_makes_perfect[blocks][1]');
-    blocklist[1].props.children.props.block.should.eql({source:"<strong>html section</strong>",
+    blocklist[1].props.children[1].props.prefix.should.equal('plastic_makes_perfect[blocks][1]');
+    blocklist[1].props.children[1].props.block.should.eql({source:"<strong>html section</strong>",
       format:"html"});
     blocklist[1].key.should.equal('1');
   });
